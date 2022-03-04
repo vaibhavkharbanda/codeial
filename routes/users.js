92a2e5bfe8da5ac667ffa44fc52ff9df2d753a02
vaibhavkharbanda <sahil031@gmail.com>
2022-03-04 16:39:58 +0530

    passport authentication

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,7 @@ router.get('/profile',userController.profile);
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
 router.post('/create',userController.create);
-router.post('/create-session',userController.createSession);
-router.get('/sign-out',userController.signOut);
+
 
 
 
