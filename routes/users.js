11ddf6c8f28c9a2940fa69c735a42895fa58216c
vaibhavkharbanda <sahil031@gmail.com>
2022-03-04 14:01:53 +0530

    manual local authentication for codeial

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,8 @@ router.get('/profile',userController.profile);
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
 router.post('/create',userController.create);
-
+router.post('/create-session',userController.createSession);
+router.get('/sign-out',userController.signOut);
 
 
 
